8efc2aa4b6fe14da6fe7ebc2d195dce618ec9ef4
frankpjr1 <34253047+frankpjr1@users.noreply.github.com>
2021-12-18 10:43:04 -0500

    I did research and declared rate as a global variable.  I also put the value of total function into variable named myAnswer and used the toFixed method to out put a two decimal value integer.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-
-function total (a,b){
-    rate = ((b-a)/a)*100;
-    rate = (0.07/a)*100;
-    rate = (0.1346)*100;
+const a = 0.52;
+const b = 0.59;
+let rate = ((b - a) / a) * 100;
+function total(a, b) {
+    rate = (0.07 / a)*100;
+    rate = (0.1346) * 100;
     return rate;
-
 }
 
-total(.52,.59);
-console.log(rate)
+total(a,b);
+const myAnswer = total(a,b);
+console.log(myAnswer.toFixed(2));
